7e7a09e19d51d5e143765dd2d7a3eb1aa9963513
JuliusFast65 <104278810+JuliusFast65@users.noreply.github.com>
2025-07-21 16:06:44 -0500

    Adición de PWA. Versión 11.

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css' // <-- ¡Esta es la línea importante!
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(registration => {
+      console.log('Service Worker registrado con scope:', registration.scope);
+    }).catch(error => {
+      console.error('Error registrando el Service Worker:', error);
+    });
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
